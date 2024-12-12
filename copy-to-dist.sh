a1d7cd2bb8e283af8b03010748110b4d82e77013
Tanay PrabhuDesai <tanayseven@gmail.com>
2024-12-12 19:55:24 +0530

    Fix the issues

--- copy-to-dist.sh
+++ copy-to-dist.sh
@@ -2,7 +2,7 @@ if [ ! -d "dist" ]; then
   mkdir dist
 fi
 
-FILES=(copy.svg skull.svg index.html style.css script.js copy-to-dist.sh unlocked.html)
+FILES=(copy.svg skull.svg index.html style.css script.js lock-screen-reminder.sh unlocked.html)
 
 for FILE in ${FILES[@]}; do
   cp $FILE dist
